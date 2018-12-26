baefffc00f541514fe7ac269fd59b26488a4e30f
minz1 <emerytang@gmail.com>
2018-12-26 13:49:57 -0600

    msm8953: remove samsung changes to fs/ecryptfs/ & security/

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -773,13 +773,6 @@ out:
 	kfree(n);
 	kfree(t);
 
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-#if !defined(CONFIG_RKP_KDP)
-	selinux_enforcing = 1;
-#endif
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enforcing)
 		return 0;
 	return -EPERM;
@@ -1540,14 +1533,6 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
-
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-#if !defined(CONFIG_RKP_KDP)
-	selinux_enforcing = 1;
-#endif
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 	if (!selinux_enforcing)
 		return 0;
 	return -EACCES;
@@ -1839,11 +1824,6 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	char *s;
 	u32 len;
 
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 	if (selinux_enforcing)
 		return -EINVAL;
 
@@ -2483,9 +2463,9 @@ out:
  * The caller must acquire the policy_rwlock before calling this function.
  */
 static inline int __security_genfs_sid(const char *fstype,
-					   char *path,
-					   u16 orig_sclass,
-					   u32 *sid)
+				       char *path,
+				       u16 orig_sclass,
+				       u32 *sid)
 {
 	int len;
 	u16 sclass;
@@ -2506,30 +2486,24 @@ static inline int __security_genfs_sid(const char *fstype,
 	}
 
 	rc = -ENOENT;
-	if (!genfs || cmp){
-		printk(KERN_ERR "SELinux: %s: genfs || cmp\n", __func__);
+	if (!genfs || cmp)
 		goto out;
-	}
 
 	for (c = genfs->head; c; c = c->next) {
 		len = strlen(c->u.name);
 		if ((!c->v.sclass || sclass == c->v.sclass) &&
-			(strncmp(c->u.name, path, len) == 0))
+		    (strncmp(c->u.name, path, len) == 0))
 			break;
 	}
 
 	rc = -ENOENT;
-	if (!c) {
-		printk(KERN_ERR "SELinux: %s empty ocontext c \n", __func__);
+	if (!c)
 		goto out;
-	}
 
 	if (!c->sid[0]) {
 		rc = sidtab_context_to_sid(&sidtab, &c->context[0], &c->sid[0]);
-		if (rc) {
-			printk(KERN_ERR "SELinux: %s: sid\n", __func__);
+		if (rc)
 			goto out;
-		}
 	}
 
 	*sid = c->sid[0];
@@ -2569,8 +2543,6 @@ int security_fs_use(struct super_block *sb)
 {
 	int rc = 0;
 	struct ocontext *c;
-	u32 tmpsid;
-
 	struct superblock_security_struct *sbsec = sb->s_security;
 	const char *fstype = sb->s_type->name;
 
@@ -2587,22 +2559,21 @@ int security_fs_use(struct super_block *sb)
 		sbsec->behavior = c->v.behavior;
 		if (!c->sid[0]) {
 			rc = sidtab_context_to_sid(&sidtab, &c->context[0],
-						   &tmpsid);
-			c->sid[0] = tmpsid;
+						   &c->sid[0]);
 			if (rc)
 				goto out;
 		}
 		sbsec->sid = c->sid[0];
 	} else {
-			rc = __security_genfs_sid(fstype, "/", SECCLASS_DIR,
-						  &sbsec->sid);
-			if (rc) {
-				sbsec->behavior = SECURITY_FS_USE_NONE;
-				rc = 0;
-			} else {
-				sbsec->behavior = SECURITY_FS_USE_GENFS;
-			}
+		rc = __security_genfs_sid(fstype, "/", SECCLASS_DIR,
+					  &sbsec->sid);
+		if (rc) {
+			sbsec->behavior = SECURITY_FS_USE_NONE;
+			rc = 0;
+		} else {
+			sbsec->behavior = SECURITY_FS_USE_GENFS;
 		}
+	}
 
 out:
 	read_unlock(&policy_rwlock);
